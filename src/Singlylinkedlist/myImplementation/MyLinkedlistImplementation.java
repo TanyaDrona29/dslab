0cd2dd2b3b2223cf59ebe8503f99f59cdc495cd7
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-01-30 16:53:08 +0545

    Singly linked list implementation

--- src/Singlylinkedlist/myImplementation/MyLinkedlistImplementation.java
+++ src/Singlylinkedlist/myImplementation/MyLinkedlistImplementation.java
@@ -1,22 +1,21 @@
 package Singlylinkedlist.myImplementation;
 
 import Singlylinkedlist.Mylinkedlist;
-import Singlylinkedlist.node.Node;
 
 public class MyLinkedlistImplementation {
     public static void main(String[] args) {
         Mylinkedlist linkedlist = new Mylinkedlist();
-        Node delete = linkedlist.deletefrombegning();
+        //Node delete = linkedlist.deletefrombegning();
         linkedlist.insertatlast(200);
         linkedlist.insertatlast(100);
         linkedlist.printlist();
         linkedlist.insertatbegning(1);
         linkedlist.printlist();
-        delete = linkedlist.deletefrombegning();
-        if (delete != null) {
-            System.out.println("deleted data = " + delete.getData());
-        } else {
-            System.out.println("empty list");
-        }
+        //delete = linkedlist.deletefrombegning();
+        //if (delete != null) {
+        //   System.out.println("deleted data = " + delete.getData());
+        //} else {
+        //   System.out.println("empty list");
+        //}
     }
 }
