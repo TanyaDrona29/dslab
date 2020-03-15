efbce51ab45f5b9e06a80da24aea7913534d078f
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-15 17:52:12 +0530

    main menu taking users input

---
+++ src/patternMenu/mainClass.java
@@ -0,0 +1,12 @@
+package patternMenu;
+
+import java.util.Scanner;
+
+public class mainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the choice");
+        int choice = scanner.nextInt();
+
+    }
+}
