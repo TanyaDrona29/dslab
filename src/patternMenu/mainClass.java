edaa07aff7298450e12ef242f573dc2080b9c8f6
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-15 17:59:42 +0530

    taking users choice

--- src/patternMenu/mainClass.java
+++ src/patternMenu/mainClass.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class mainClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the choice");
+        System.out.println("welcome to menu driven pattern program");
+        System.out.println("Press 1 : to display a rectangle pattern\n" +
+                "Press 2 : to display a square pattern\n" +
+                "Press 3 : to display a right angled lower triangle\n" +
+                "press 4 : to display a right angled upper triangle");
         int choice = scanner.nextInt();
 
     }
