7f9166057a29cf0bb3e72286b941e9a1fa5c8518
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-16 18:09:30 +0530

    user input and menu display

--- src/patternMenu/mainClass.java
+++ src/patternMenu/mainClass.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class mainClass {
     public static void main(String[] args) {
+        patternClass patternClass = new patternClass();
         Scanner scanner = new Scanner(System.in);
         System.out.println("welcome to menu driven pattern program");
         System.out.println("Press 1 : to display a rectangle pattern\n" +
