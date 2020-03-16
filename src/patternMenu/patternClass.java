9f64eaea0275ad32f299510733d3a669ab2c8889
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-16 17:11:55 +0530

    method to print rectangle using * symbol

---
+++ src/patternMenu/patternClass.java
@@ -0,0 +1,13 @@
+package patternMenu;
+
+public class patternClass {
+    public void printRectangle(int length, int breadth) {
+        for (int i = 1; i <= length; i++) {
+            for (int j = 1; j <= breadth; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
