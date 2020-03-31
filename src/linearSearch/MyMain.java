637903b3a370abdc95e9d107669599f4a6772d99
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-31 12:09:52 +0530

    taking length of arrary

---
+++ src/linearSearch/MyMain.java
@@ -0,0 +1,12 @@
+package linearSearch;
+
+import java.util.Scanner;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter length");
+        int length = scanner.nextInt();
+
+    }
+}
