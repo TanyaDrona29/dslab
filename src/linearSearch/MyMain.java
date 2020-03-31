bde6190c18491dc741b30cb4519328c2c829cc7e
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-31 12:10:56 +0530

    taking arrary elements from user

--- src/linearSearch/MyMain.java
+++ src/linearSearch/MyMain.java
@@ -7,6 +7,11 @@ public class MyMain {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter length");
         int length = scanner.nextInt();
+        int arr[] = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.println("enter element");
+            arr[i] = scanner.nextInt();
+        }
+
 
     }
-}
