9046478b73f77e0b06561ee7d41ae1bf20772cd8
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-04-09 15:43:50 +0530

    code to print rectangle

--- src/patternMenu/patternClass.java
+++ src/patternMenu/patternClass.java
@@ -10,32 +10,4 @@ public class patternClass {
         }
     }
 
-    public void printSquare(int lengthSquare) {
-        for (int i = 1; i <= lengthSquare; i++) {
-            for (int j = 1; j <= lengthSquare; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    public void lowerTriangle(int height) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    public void upperTriangle(int size) {
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                if (i <= j) {
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-        }
-    }
 }
