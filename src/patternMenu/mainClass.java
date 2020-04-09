b082055e06424b90c5e5c557bea94e7f630c31f6
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-04-09 15:47:15 +0530

    printing main menu for user to select

--- src/patternMenu/mainClass.java
+++ src/patternMenu/mainClass.java
@@ -12,35 +12,5 @@ public class mainClass {
                 "Press 3 : to display a right angled lower triangle\n" +
                 "press 4 : to display a right angled upper triangle");
         int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("you have choose to print a rectangle");
-                System.out.println("enter length of rectangle.");
-                int length = scanner.nextInt();
-                System.out.println("enter breadth of rectangle.");
-                int breadth = scanner.nextInt();
-                patternClass.printRectangle(length, breadth);
-                break;
-            case 2:
-                System.out.println("you have choosen to print a square");
-                System.out.println("enter length of square.");
-                int lengthSquare = scanner.nextInt();
-                patternClass.printSquare(lengthSquare);
-                break;
-            case 3:
-                System.out.println("you have choosen to print a lower triangle");
-                System.out.println("enter height of triangle.");
-                int height = scanner.nextInt();
-                patternClass.lowerTriangle(height);
-                break;
-            case 4:
-                System.out.println("you have choosen to print a upper triangle");
-                System.out.println("enter height of triangle");
-                int size = scanner.nextInt();
-                patternClass.upperTriangle(size);
-                break;
-            default:
-                System.out.println("Invalid option!! I am sorry");
-        }
+
     }
-}
